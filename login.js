7a4f7854c86d21f693b8a8a3508534ee9444cda0
doqso <giorgi.gongadze.92@gmail.com>
2022-01-19 17:21:46 +0100

    Las funciones de almacenamiento de usuarios implementadas

---
+++ login.js
@@ -0,0 +1,18 @@
+// Obtener todos los usuarios
+function getUsers() {
+  localStorage.users = localStorage.users || JSON.stringify([]);
+  return JSON.parse(localStorage.users);
+}
+
+// Obtener un usuario en específico
+function getUser(username) {
+  let allUsers = getUsers();
+  return allUsers.find((user) => user.username === username);
+}
+
+// Añadir usuario al almacenamiento del navegador
+function addUser({ username, pass }) {
+  let allUsers = getUsers();
+  allUsers.push({ username, pass });
+  localStorage.users = JSON.stringify(allUsers);
+}
