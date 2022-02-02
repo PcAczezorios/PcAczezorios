d49d4504108436a702d8a9d4158131f7b66a271f
JesusFerVid <jesusfervid94@gmail.com>
2022-02-02 09:45:03 +0100

    Decimales
    Se muestran los decimales, siempre 2 cifras.
    Refactorización leve en carrito.

--- js/carrito.js
+++ js/carrito.js
@@ -39,7 +39,7 @@ function comprarTodoClickado(){
 
 function eliminarDelCarrito(event){
     var botonClickado = event.target
-    botonClickado.parentElement.parentElement.remove()
+    botonClickado.parentElement.remove()
     actualizarPrecioTotalCarrito()
 }
 
@@ -48,15 +48,15 @@ function añadirAlCarritoClickado(event){
     var articuloTienda = botonClickado.parentElement.parentElement
     var titulo = articuloTienda.getElementsByClassName('nombre-producto')[0].innerText
     console.log(titulo)
-    var precio = parseFloat(articuloTienda.getElementsByClassName('precio-producto')[0].innerText.replace('€',''))
+	var precio = parseFloat(articuloTienda.getElementsByClassName('precio-producto')[0].innerText.replace(',', '.').replace(' €', '')).toFixed(2)
     var imagenSrc = articuloTienda.getElementsByClassName('imagen-articulo')[0].src
-    añadirItemAlCarrito(titulo, precio, imagenSrc)
+    añadirItemAlCarrito(titulo, precio.toString().replace('.', ','), imagenSrc)
     actualizarPrecioTotalCarrito()
 }
 
 function añadirItemAlCarrito(titulo, precio, imagenSrc){
     var carritoItem = document.createElement('div')
-    carritoItem.classList.add('producto')
+    carritoItem.classList.add('articulo-en-carrito')
     var objetosCarrito = document.getElementsByClassName('objetos-carrito')[0]
     var objetosCarritoNombres = objetosCarrito.getElementsByClassName('nombre-producto')
     for (var i = 0; i < objetosCarritoNombres.length; i++){
@@ -66,14 +66,12 @@ function añadirItemAlCarrito(titulo, precio, imagenSrc){
             return
         }
     }
-    var carritoItemContenido = `
-        <div class="articulossss">
-            <img src="${imagenSrc}" width="40" height="40" class="imagen-articulo">
-            <span class="nombre-producto">${titulo}</span>
-        <span class="precio-producto ariculossss">${precio}€</span>
-            <input type="number" min="1" value="1" class="cantidad-producto">
-            <button class="borrar-carrito">Borrar</button>
-        </div>`
+    var carritoItemContenido = 
+		`<img src="${imagenSrc}" width="128" height="128" class="imagen-articulo">
+		<span class="nombre-producto">${titulo}</span>
+		<span class="precio-producto">${precio} €</span>
+		<input type="number" min="1" value="1" class="cantidad-producto">
+		<button class="borrar-carrito">Borrar</button>`
     carritoItem.innerHTML = carritoItemContenido
     objetosCarrito.append(carritoItem)
     carritoItem.getElementsByClassName('borrar-carrito')[0].addEventListener('click', eliminarDelCarrito)
@@ -90,16 +88,16 @@ function cambioCantidad(event){
 
 function actualizarPrecioTotalCarrito(){
     var contenedorObjetosCarrito = document.getElementsByClassName('objetos-carrito')[0]
-    var productos = contenedorObjetosCarrito.getElementsByClassName('producto')
+    var productos = contenedorObjetosCarrito.getElementsByClassName('articulo-en-carrito')
     var total = 0;
     for (var i = 0; i < productos.length; i++){
         var carritoItem = productos[i]
         var precioElemento = carritoItem.getElementsByClassName('precio-producto')[0]
         var cantidadProductos = carritoItem.getElementsByClassName('cantidad-producto')[0]
         var cantidad = cantidadProductos.value
-        var precio = parseFloat(precioElemento.textContent.replace(' €', ''))
+        var precio = parseFloat(precioElemento.textContent.replace(',', '.').replace(' €', '')).toFixed(2)
         total = total + (precio * cantidad)
     }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('precio-total')[0].innerText = total + " €"
+    total = (Math.round(total * 100) / 100).toFixed(2)
+    document.getElementsByClassName('precio-total')[0].innerText = total.toString().replace('.', ',') + " €"
 }
